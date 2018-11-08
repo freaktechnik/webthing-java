35aa734afbb662cde39131d15547dc0e915d1e53
Martin Giger <martin@humanoids.be>
2018-11-08 15:48:07 +0100

    Try different approach to check for % in address

--- src/main/java/org/mozilla/iot/webthing/Utils.java
+++ src/main/java/org/mozilla/iot/webthing/Utils.java
@@ -57,6 +57,6 @@ public class Utils {
     }
 
     private static boolean isValidAddress(InetAddress address) {
-        return !address.isLoopbackAddress() && !address.isMulticastAddress() && address.getHostAddress().indexOf("%") == 0;
+        return !address.isLoopbackAddress() && !address.isMulticastAddress() && !address.getHostAddress().contains("%");
     }
 }
